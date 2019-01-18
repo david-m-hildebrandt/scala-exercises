d7eb1fbc6bd73c326216819cac7cf7fa01054922
David M. Hildebrandt <david.m.hildebrandt@gmail.com>
2019-01-18 16:58:01 -0500

    Completed work on the mapping exercise

--- src/main/scala/com/lantern/study/questions/Solutions.scala
+++ src/main/scala/com/lantern/study/questions/Solutions.scala
@@ -11,16 +11,113 @@ object Solutions {
     * Count letter occurrences in a String
     */
 
-  def countChars(s : String) : List[(Char, Int)] = {
+  def countChars(s: String): List[(Char, Int)] = {
 
-    val map = mutable.Map[Char,Int]()
+    val map = mutable.Map[Char, Int]()
     s.foreach(c => {
-      map.put(c,  1 +  map.getOrElse(c, 0))
+      map.put(c, 1 + map.getOrElse(c, 0))
     })
 
     map.toList
   }
 
+  def main(a: Array[String]): Unit = {
+
+    val letterMap = Map(
+      'a' -> 4, 'A' -> 4,
+      'e' -> 3, 'E' -> 3,
+      'i' -> 1, 'I' -> 1,
+      'o' -> 0, 'O' -> 0,
+      's' -> 5, 'S' -> 5,
+      't' -> 7, 'T' -> 7)
+
+ //   println(stringMapper("hello", letterMap))
+    println(countLetters2("aaabbbc"))
+  }
+
+  def stringMapper(input: String, letterMap: Map[Char, Int]): String = {
+    input.map(c => {
+      letterMap.getOrElse(c, c)
+    }).mkString("")
+  }
+
+
+  def countLetters(s: String): String = {
+
+    if (s == null || s == "") {
+      ""
+    }
+
+    var counter: Int = 1
+    var currentChar: String = s.substring(0, 1)
+    var output: String = ""
+
+    s.substring(1).foreach {
+      c => {
+        println(c)
+        if (("" + c) == currentChar) {
+          counter = counter + 1
+        } else {
+          output = output + currentChar + counter
+          currentChar = "" + c
+          counter = 1
+        }
+      }
+    }
+
+    output + currentChar + counter
+  }
+
+
+  def countLetters2(s: String): String = {
+
+    if (s == null || s == "") {
+      ""
+    }
+
+    var counter: Int = 1
+    var currentChar: Char = s.substring(0, 1).toCharArray.head
+    var output: String = ""
+
+    s.substring(1).foreach {
+      c => {
+        if (c == currentChar) {
+          counter = counter + 1
+        } else {
+          output = output + currentChar + counter
+          currentChar = c
+          counter = 1
+        }
+      }
+    }
+
+    output + currentChar + counter
+  }
+
+  def countLetters3(s: String): String = {
+
+    if (s == null || s == "") {
+      ""
+    }
+
+    var counter: Int = 1
+    var currentChar: Char = s.substring(0, 1).toCharArray.head
+    var output: String = ""
+
+    s.substring(1).foreach {
+      c => {
+        if (c == currentChar) {
+          counter = counter + 1
+        } else {
+          output = output + currentChar + counter
+          currentChar = c
+          counter = 1
+        }
+      }
+    }
+
+    output + currentChar + counter
+  }
 
   /**
     * Map characters
--- src/test/scala/com/lantern/study/questions/SolutionsTestSuite.scala
+++ src/test/scala/com/lantern/study/questions/SolutionsTestSuite.scala
@@ -1,9 +1,9 @@
 package com.lantern.study.questions
 
+import com.lantern.study.questions.Solutions._
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
-import com.lantern.study.questions.Solutions._
 
 @RunWith(classOf[JUnitRunner])
 class SolutionsTestSuite extends FunSuite {
@@ -13,6 +13,7 @@ class SolutionsTestSuite extends FunSuite {
     val s1 = "abcdeabcde"
 
     assert(countChars(s1).contains(('a', 2)))
+    println(countChars(s1))
 
   }
 }
